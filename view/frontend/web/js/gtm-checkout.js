8bdee8dfb4a4a9dc3ca080348a244abbc56998e5
Aleksej Tsebinoga <aleksejt@scandiweb.com>
2017-10-09 12:35:17 +0200

    Restructure checkout library. Add safe checks for the variables

--- view/frontend/web/js/gtm-checkout.js
+++ view/frontend/web/js/gtm-checkout.js
@@ -10,22 +10,40 @@ require(['jquery'],
 
     function ($) {
 
+        if (isDataLayer()) {
+            main();
+        }
+
         /**
-         * Runs the logic, if dataLayer object is defined
+         * Runs application logic
+         *
+         * @returns {boolean}
          */
-        $(document).ready(function () {
-            if (isDataLayer()) {
+        function main() {
+            if (!isDataReady()) {
+                setTimeout(function () {
+                    main();
+                }, 1000);
+                return false;
+            }
+            $(document).ready(function () {
                 collectCheckoutPush();
                 bindInputs();
                 $(window).bind('hashchange', function (e) {
                     collectCheckoutPush();
                     bindInputs();
                 });
-                $(checkoutWrappers[0]).bind("DOMSubtreeModified",function(){
-                    alert('changed');
-                });
-            }
-        });
+            });
+        }
+
+        /**
+         * Check if all require data was initialised
+         *
+         * @returns {boolean}
+         */
+        function isDataReady() {
+            return !getCheckoutWrappers() && !getCart() && !getSteps();
+        }
 
         /**
          * etect checkout step, based on the array
@@ -43,6 +61,9 @@ require(['jquery'],
             else if (steps.includes(anchor)) {
                 step = steps.indexOf(anchor) + 1;
             }
+            else if (typeof (step) === 'undefined') {
+                return false;
+            }
             return step.toString();
         }
 
@@ -52,20 +73,6 @@ require(['jquery'],
          */
         function isDataLayer() {
             return typeof(dataLayer) !== 'undefined';
-        }
-
-        /**
-         * Get global with steps from the backend.
-         * Generated in Scandi\Gtm\Helper\Collectors\Checkout->getCheckoutSteps;
-         * @returns {*}
-         */
-        function getSteps() {
-            if (typeof(checkoutLayerSteps) !== 'undefined') {
-                return checkoutLayerSteps;
-            }
-            else {
-                return false;
-            }
         }
 
         /**
@@ -102,31 +109,28 @@ require(['jquery'],
         }
 
         /**
-         * Find cart data pushed on page render
-         * @returns {*}
-         */
-        function getCart() {
-            // Generated in Scandi\Gtm\Helper\Collectors\Checkout->getCart();
-            if (typeof(cartData) !== 'undefined') {
-                return cartData;
-            }
-            return '';
-        }
-
-        /**
          * Bind radio buttons with push
+         *
          * @returns {null}
          */
         function bindInputs() {
             var inputs = getInputsByStep();
-            if (inputs.length === 0) {
-                setTimeout(function(){
+            if (!inputs) {
+                setTimeout(function () {
                     bindInputs()
                 }, 1000);
                 return null;
             }
-            setTimeout(function(){
-                jQuery(inputs).change(function(){collectCheckoutPush($(this).attr('value'))})
+            if (inputs.length === 0) {
+                setTimeout(function () {
+                    bindInputs()
+                }, 1000);
+                return null;
+            }
+            setTimeout(function () {
+                jQuery(inputs).change(function () {
+                    collectCheckoutPush($(this).attr('value'))
+                })
             }, 1000);
         }
 
@@ -135,14 +139,20 @@ require(['jquery'],
          * @returns {*}
          */
         function getInputsByStep() {
-            var wrappers = getCheckoutWrappers(), index = detectCheckoutStep() - 1;
-            if (wrappers.length <= 0) {
+            var wrappers = getCheckoutWrappers(), index = detectCheckoutStep();
+            if (!wrappers) {
                 return null;
             }
-            if (wrappers[index] === 'undefined') {
+            if (!index) {
                 return null;
             }
-            var selector = wrappers[index] + ' input[type=radio]';
+            if (wrappers[index - 1] === 'undefined') {
+                return null;
+            }
+            if (wrappers[index - 1].length === 0) {
+                return null;
+            }
+            var selector = wrappers[index - 1] + ' input[type=radio]';
             return $(selector);
         }
 
@@ -155,6 +165,31 @@ require(['jquery'],
             if (typeof checkoutWrappers !== 'undefined') {
                 return checkoutWrappers;
             }
-            return '';
+            return null;
         }
+
+        /**
+         * Find cart data pushed on page render
+         * @returns {*}
+         */
+        function getCart() {
+            // Generated in Scandi\Gtm\Helper\Collectors\Checkout->getCart();
+            if (typeof(cartData) !== 'undefined') {
+                return cartData;
+            }
+            return null;
+        }
+
+        /**
+         * Get global with steps from the backend.
+         * Generated in Scandi\Gtm\Helper\Collectors\Checkout->getCheckoutSteps;
+         * @returns {*}
+         */
+        function getSteps() {
+            if (typeof(checkoutLayerSteps) !== 'undefined') {
+                return checkoutLayerSteps;
+            }
+            return null;
+        }
+
     });
